f9db5c2986f008e3382f5de1b1f5f5bdb3947bc7
yhassanzadeh13@ku.edu.tr <yhassanzadeh13@ku.edu.tr>
2018-12-23 08:32:05 +0200

    Spring initializer added

---
+++ spring5web/src/main/java/com/yhassanzadeh/spring5web/Spring5webApplication.java
@@ -0,0 +1,16 @@
+package com.yhassanzadeh.spring5web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Spring5webApplication.class, args);
+    }
+
+}
+
